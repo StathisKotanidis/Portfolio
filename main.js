cfe7312141d9d8bc425facd6b65fdda8de54bd86
StathisKotanidis <stathiskotanidhs@gmail.com>
2024-07-18 18:52:00 +0300

    Made the first version ofthe about section, didnt make it responsive

--- main.js
+++ main.js
@@ -1,10 +1,6 @@
 let menuBtn = document.querySelector(".menu-icon");
 let sideNavbar = document.querySelector(".side-navbar");
 
-// menuBtn.addEventListener("click", () => {
-//   sideNavbar.style.display = "flex";
-// });
-
 menuBtn.addEventListener("click", () => {
   sideNavbar.classList.toggle("hidden");
 });
