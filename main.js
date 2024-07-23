fba87565e58803be000f19d68db8954985f12ad6
StathisKotanidis <stathiskotanidhs@gmail.com>
2024-07-23 19:58:56 +0300

    finished styling the project section for now

--- main.js
+++ main.js
@@ -5,3 +5,14 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+
+let calculatorLiveBtn = document.querySelector("#calculator-live");
+let calculatorGitBtn = document.querySelector("#calculator-source");
+
+calculatorLiveBtn.addEventListener("click", () => {
+  window.open("https://stathiskotanidis.github.io/calculator/", "_blank");
+});
+
+calculatorGitBtn.addEventListener("click", () => {
+  window.open("https://github.com/StathisKotanidis/calculator", "_blank");
+});
