496428d308272c310b3c53407e9441126ae637c0
StathisKotanidis <stathiskotanidhs@gmail.com>
2024-08-01 22:41:27 +0300

    Started from 0, mobile ready so far

--- main.js
+++ main.js
@@ -1,10 +1,9 @@
-function toggleMenu() {
-  const menu = document.querySelector(".menu-links");
-  const icon = document.querySelector(".menu-icon");
-
-  menu.classList.toggle("open");
-  icon.classList.toggle("open");
-}
+let menuBtn = document.querySelector(".menu-icon");
+let menuLinks = document.querySelector(".menu-links");
+menuBtn.addEventListener("click", () => {
+  console.log("works");
+  menuLinks.classList.toggle("hidden");
+});
 
 let calculatorLiveBtn = document.querySelector("#calculator-live");
 let calculatorGitBtn = document.querySelector("#calculator-source");
@@ -15,4 +14,21 @@ calculatorLiveBtn.addEventListener("click", () => {
 
 calculatorGitBtn.addEventListener("click", () => {
   window.open("https://github.com/StathisKotanidis/calculator", "_blank");
+});
+
+let restaurantLiveBtn = document.querySelector("#restaurant-live");
+let restaurantGitBtn = document.querySelector("#restaurant-source");
+
+restaurantLiveBtn.addEventListener("click", () => {
+  window.open(
+    "https://stathiskotanidis.github.io/Restaurant-layout/",
+    "_blank"
+  );
+});
+
+restaurantGitBtn.addEventListener("click", () => {
+  window.open(
+    "https://github.com/StathisKotanidis/Restaurant-layout",
+    "_blank"
+  );
 });
